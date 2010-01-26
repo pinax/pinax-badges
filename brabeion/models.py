54b290a191766ed4d0428e0a73246239f6cfd836
Alex Gaynor <alex.gaynor@gmail.com>
2010-01-26 16:40:54 -0500

    Change imports to conform to the pinax style guide.

--- brabeion/models.py
+++ brabeion/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from django.contrib.auth.models import User
 from django.db import models
+
+from django.contrib.auth.models import User
 
 
 class BadgeAwarded(models.Model):
