ab9c7649165ff863fb08eee85fb664161bf081f9
Brian Rosner <brosner@gmail.com>
2010-02-22 23:51:52 -0500

    fixed URL view name and name

--- brabeion/urls.py
+++ brabeion/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns("",
-    url(r"^$", "brabeion.views.list_badges", name="badges"),
+    url(r"^$", "brabeion.views.badge_list", name="badge_list"),
     url(r"^(\w+)/(\d+)/$", "brabeion.views.badge_detail", name="badge_detail"),
 )
