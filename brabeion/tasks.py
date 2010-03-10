0f8e51c2423d88a52d27cb6576489d1bcbc7602c
Alex Gaynor <alex.gaynor@gmail.com>
2010-03-10 02:55:17 -0600

    Added ignore_result to the celery task.

--- brabeion/tasks.py
+++ brabeion/tasks.py
@@ -3,5 +3,7 @@ from celery.task import Task
 
 
 class AsyncBadgeAward(Task):
+    ignore_result = True
+    
     def run(self, badge, state, **kwargs):
         badge.actually_possibly_award(**state)
