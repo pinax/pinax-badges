d2bef61f0b9d3638eaf6388d3380b0c1176936f2
Brian Rosner <brosner@gmail.com>
2010-07-06 15:54:37 -0600

    0.1.dev12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "brabeion",
-    version = "0.1.dev11",
+    version = "0.1.dev12",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "a reusable Django badges application",
