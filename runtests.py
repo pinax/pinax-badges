f85895ab7b33a92b4f0a4ea58769bf84df89ecc7
Bahattin Cinic <bahattin.cinic@metglobal.com>
2015-01-05 01:13:47 +0200

    Fixed warnings for django 1.7

--- runtests.py
+++ runtests.py
@@ -17,6 +17,13 @@ settings.configure(
         "brabeion",
         "brabeion.tests",
     ],
+    # this fixes warnings in django 1.7
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    )
 )
 
 
