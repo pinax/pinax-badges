3664a8704490adafc55bb0aff30ec1f04be9829f
Rick Vause <rvause@gmail.com>
2015-04-23 20:15:57 +0200

    Update initial migration

--- brabeion/migrations/0001_initial.py
+++ brabeion/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
 from django.conf import settings
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='BadgeAward',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('awarded_at', models.DateTimeField(default=datetime.datetime.now)),
+                ('awarded_at', models.DateTimeField(default=django.utils.timezone.now)),
                 ('slug', models.CharField(max_length=255)),
                 ('level', models.IntegerField()),
                 ('user', models.ForeignKey(related_name='badges_earned', to=settings.AUTH_USER_MODEL)),
