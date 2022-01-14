ac65c169dd0a8842a9350f3804afe4b0dd925daa
Pavel Břoušek <melanger@users.noreply.github.com>
2022-01-14 10:53:50 +0100

    chore: bump version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.0.0"
+VERSION = "3.0.1"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-badges.svg
     :target: https://pypi.python.org/pypi/pinax-badges/
